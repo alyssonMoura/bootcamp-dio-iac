d3fc584af9357e269ac924fefde0a89f588baeb2
AlyssonMoura <alyssonmoura367@gmail.com>
2023-05-26 00:04:10 -0300

    Criando usuarios

--- infra.sh
+++ infra.sh
@@ -12,3 +12,16 @@ echo "Criando grupos"
 groupadd GRP_ADM
 groupadd GRP_VAN
 groupadd GRP_SEC
+
+echo "Criar usuários"
+
+useradd calos -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Abc123)
+
